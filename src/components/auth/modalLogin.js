1ef50046de1563610f3aa999dfa71b390d4a42c5
antonnygit <antonnygithub@gmail.com>
2024-12-04 22:55:23 -0300

    login e registro integrado com api

--- src/components/auth/modalLogin.js
+++ src/components/auth/modalLogin.js
@@ -1,15 +1,53 @@
-import { Button, Form, FormGroup } from 'react-bootstrap';
+import { Button, Form, FormGroup, Spinner } from 'react-bootstrap';
 import './../../styles/modalAuth.css';
+import { useState } from 'react';
 
+import * as Yup from 'yup';
+import LoginService from '../../services/auth/login';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const handleSuapBtnClick = () => {
+    let responseType = 'token';
+    let grantType = 'implicit';
+    const clientId = process.env.REACT_APP_SUAP_CLIENT_ID;
+    window.location.href = `https://suap.ifrn.edu.br/o/authorize?response_type=${responseType}&grant_type=${grantType}&client_id=${clientId}`;
+}
+
+const validationSchema = Yup.object({
+    email: Yup.string().email('Email inválido').required('Email obrigatório'),
+    password: Yup.string().required('Senha obrigatória'),
+}).required();
 
 const ModalLogin = () => {
-    const handleSuapBtnClick = () => {
-        let responseType = 'token';
-        let grantType = 'implicit';
-        const clientId = "xDxhIYTbk9cLpdP7hR3Kvc8lx1iKzWpmrNYZovwC";
-        window.location.href = `https://suap.ifrn.edu.br/o/authorize?response_type=${responseType}&grant_type=${grantType}&client_id=${clientId}`;
-    }
+    const [loadingLogin, setLoadingLogin] = useState(false);
 
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = async (data) => {
+        setLoadingLogin(true);
+
+        try {
+            const response = await LoginService(data.email, data.password);
+            const token = response.data.token;
+            localStorage.clear();
+            localStorage.setItem('token', token);
+            window.location.href = '/vehicles';
+        } catch (err) {
+            if (err.response) {
+                if (err.response.status === 401)
+                    alert('Email ou senha inválidos');
+            }
+            else {
+                console.log(err);
+                alert('Erro ao fazer login');
+            }
+        } finally {
+            setLoadingLogin(false);
+        }
+    }
 
     return (
         <div className='modal-auth p-5 mx-auto mt-5 rounded-4' style={{ display: 'block' }}>
@@ -22,22 +60,36 @@ const ModalLogin = () => {
                                 <h1 className='fw-bold text-danger'>Não possui uma conta?</h1>
                                 <Button className='fw-bolder' href='/register' variant='outline-danger'>Cadastre-se</Button>
                             </div>
-                            
+
                             {/* Lado direito */}
                             <div className="col-lg-6 border-start">
                                 <h1 className='fw-bold text-danger'>Login</h1>
                                 <p>Entre com seu email e senha</p>
-                                <Form>
+                                <Form onSubmit={handleSubmit(onSubmit)}>
                                     <FormGroup className="mb-3" controlId="loginForm.EmailInput">
                                         <Form.Label className='text-danger fw-bolder'>Email</Form.Label>
-                                        <Form.Control type='email' placeholder='example@example.com' className='rounded-0' />
+                                        <Form.Control {...register('email')} className={`rounded-0 ${errors.email ? 'is-invalid' : ''}`} type='email' placeholder='example@example.com' />
+                                        {errors.email && (
+                                            <div className="invalid-feedback d-block">
+                                                {errors.email.message}
+                                            </div>
+                                        )}
                                     </FormGroup>
                                     <FormGroup className="mb-3" controlId="loginForm.PasswordInput">
                                         <Form.Label className='text-danger fw-bolder'>Senha</Form.Label>
-                                        <Form.Control type='password' placeholder='**********' className='rounded-0' />
+                                        <Form.Control {...register('password')} className={`rounded-0 ${errors.password ? 'is-invalid' : ''}`} type='password' placeholder='**********' />
+                                        {errors.password && (
+                                            <div className="invalid-feedback d-block">
+                                                {errors.password.message}
+                                            </div>
+                                        )}
                                     </FormGroup>
                                     <div className='d-flex'>
-                                        <Button variant='danger' className='fw-bolder text-body-light' type='submit'>Entrar</Button>
+                                        {loadingLogin ? (
+                                            <Button variant='danger' className='fw-bolder text-body-light' type='submit'><Spinner animation="border" size="sm" variant='light' /></Button>
+                                        ) : (
+                                            <Button variant='danger' className='fw-bolder text-body-light' type='submit'>Entrar</Button>
+                                        )}
                                         <Button variant='light' className='ms-1' onClick={handleSuapBtnClick}>Entrar com suap</Button>
                                     </div>
                                 </Form>
--- src/components/auth/modalRegister.js
+++ src/components/auth/modalRegister.js
@@ -1,8 +1,42 @@
-import { Button, Form, FormGroup } from 'react-bootstrap';
+import { Button, Form, FormGroup, Spinner } from 'react-bootstrap';
 import './../../styles/modalAuth.css';
 
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import RegisterService from '../../services/auth/register';
+
+const validationSchema = Yup.object({
+    name: Yup.string().required('Nome obrigatório'),
+    email: Yup.string().email('Email inválido').required('Email obrigatório'),
+    password: Yup.string().required('Senha obrigatória'),
+}).required();
 
 const ModalRegister = () => {
+    const [loadingRegister, setLoadingRegister] = useState(false);
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = async (data) => {
+        const didRegister = async () => {
+            try {
+                setLoadingRegister(true);
+                const response = await RegisterService(data.name, data.email, data.password);
+                window.location.href = '/login';
+            } catch (err) {
+                alert('Erro ao cadastrar');
+                console.log(err);
+            } finally{
+                setLoadingRegister(false);
+            }
+        }
+
+        didRegister();
+    }
+
     return (
         <div className='modal-auth p-5 mx-auto mt-5 rounded-4' style={{ display: 'block' }}>
             <div className="modal-dialog modal-dialog-centered" role="document">
@@ -19,21 +53,40 @@ const ModalRegister = () => {
                             <div className="col-lg-6 border-start" >
                                 <h1 className='fw-bold text-danger'>Cadastre-se</h1>
                                 <p>Crie uma conta com seu email</p>
-                                <Form>
+                                <Form onSubmit={handleSubmit(onSubmit)}>
                                     <FormGroup className="mb-3" controlId="loginForm.NameInput">
                                         <Form.Label className='text-danger fw-bolder'>Nome</Form.Label>
-                                        <Form.Control type='text' placeholder='João' />
+                                        <Form.Control {...register('name')} className={`rounded-0 ${errors.name ? 'is-invalid' : ''}`} type='text' placeholder='João' />
+                                        {errors.name && (
+                                            <div className="invalid-feedback d-block">
+                                                {errors.name.message}
+                                            </div>
+                                        )}
                                     </FormGroup>
                                     <FormGroup className="mb-3" controlId="loginForm.EmailInput">
                                         <Form.Label className='text-danger fw-bolder'>Email</Form.Label>
-                                        <Form.Control type='email' placeholder='example@example.com' />
+                                        <Form.Control {...register('email')} className={`rounded-0 ${errors.email ? 'is-invalid' : ''}`} type='email' placeholder='example@example.com' />
+                                        {errors.email && (
+                                            <div className="invalid-feedback d-block">
+                                                {errors.email.message}
+                                            </div>
+                                        )}
                                     </FormGroup>
                                     <FormGroup className="mb-3" controlId="loginForm.PasswordInput">
                                         <Form.Label className='text-danger fw-bolder'>Senha</Form.Label>
-                                        <Form.Control type='password' placeholder='**********' />
+                                        <Form.Control {...register('password')} className={`rounded-0 ${errors.password ? 'is-invalid' : ''}`} type='password' placeholder='**********' />
+                                        {errors.password && (
+                                            <div className="invalid-feedback d-block">
+                                                {errors.password.message}
+                                            </div>
+                                        )}
                                     </FormGroup>
+                                    {loadingRegister ? (
+                                        <Button variant='danger' className='fw-bolder text-body-light' type='submit'><Spinner animation="border" size="sm" variant='light' /></Button>
+                                    ) : (
+                                        <Button variant='danger' className='fw-bolder text-body-light' type='submit'>Criar conta</Button>
+                                    )}
                                 </Form>
-                                <Button variant='danger' className='fw-bolder text-body-light' type='submit'>Criar conta</Button>
                             </div>
                         </div>
                     </div>
