0cca585f85a41829328991f3365b35124868475e
antonnygit <antonnygithub@gmail.com>
2024-12-04 23:02:03 -0300

    instancia base para requisicoes

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+const host = process.env.REACT_APP_API_HOST;
+const port = process.env.REACT_APP_API_PORT;
+
 const AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
-    timeout: 30000
+    baseURL: `${host}:${port}/api/v1`,
+    timeout: 30000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 export default AxiosInstance;
