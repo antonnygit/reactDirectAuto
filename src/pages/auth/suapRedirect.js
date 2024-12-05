61b9c0349f8bba8582c24a0bda25ce3398d8e615
antonnygit <antonnygithub@gmail.com>
2024-12-04 22:57:17 -0300

    pagina para pegar dados do suap e fazer login

--- src/pages/auth/suapRedirect.js
+++ src/pages/auth/suapRedirect.js
@@ -1,53 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import GetUserDataSuapService from "../../services/auth/suap";
+import { Spinner } from "react-bootstrap";
+import AuthFromSuapService from "../../services/auth/registerSuap";
 
 
 // os dados devem ser pegos no backend
 const SuapRedirect = () => {
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [accessToken, setAccessToken] = useState(null);
 
-    useEffect(() => {
-        const hash = window.location.hash;
-        const params = new URLSearchParams(hash.replace("#", ''));
-    
-        const access_token = params.get('access_token');
-    
-        if (!access_token)
-            window.location.href = '/';
+    useEffect(async () => {
+        const fetchData = async () => {
+            const hash = window.location.hash;
+            const params = new URLSearchParams(hash.replace("#", ''));
+            const access_token = params.get('access_token');
 
-        setAccessToken(access_token);
+            if (!access_token) {
+                window.location.href = '/';
+                return;
+            }
+
+            try {
+                const response = await GetUserDataSuapService(access_token);
+                const { nome, email_preferencial, foto } = response.data;
+
+                const responseSuap = await AuthFromSuapService(nome, email_preferencial, foto);
+                const authToken = responseSuap.data.token;
+                localStorage.setItem('token', authToken);
+                window.location.href = '/vehicles';
+            } catch (err) {
+                window.location.href = "/";
+            }
+        };
+
+        fetchData();
     }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const response = await GetUserDataSuapService(accessToken);
-                setUserData(response.data);
-            }catch(err)
-            {
-                setError(err);
-            }finally{
-                setLoading(false);
-            }
-        }
-
-        if(accessToken)
-            fetchData();
-
-    }, [accessToken]);
-
-    if(loading)
-        return <p>Carregando...</p>
-
-    return (
-        <div>
-            <h1>{userData ? `Matrícula: ${userData.identificacao}` : `No Data`}</h1>
-            <p>Token: {accessToken}</p>
-        </div>
-    )
+    return <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}><Spinner animation="border" variant="danger"></Spinner></div>
 }
 
 export default SuapRedirect;
